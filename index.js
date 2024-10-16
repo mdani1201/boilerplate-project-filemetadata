f2b525ae7f0dbe3123bda6a265516dfdf6512cb6
mdani1201 M <modani1201@gmail.com>
2024-10-16 14:24:32 +0000

    with the help of multer documentation

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
-var express = require('express');
-var cors = require('cors');
-require('dotenv').config()
-const multer  = require('multer')
-const upload = multer({ dest: 'uploads/' })
+var express = require("express");
+var cors = require("cors");
+require("dotenv").config();
+const multer = require("multer");
+const upload = multer().single('upfile');
+
 
 var app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 
-app.post("/", upload.single('upfile'),(req, res)=> {
-    console.log(...req.file);
+app.post('/api/fileanalyse', function (req, res) {
+  upload(req, res, function (err) {
+    if (err instanceof multer.MulterError) {
+      console.log(err);
+    } else if (err) {
+      console.log(err);
+    }
+    const {originalname, size, mimetype} = req.file;
+    console.log("Upload successful");
+    res.json({name: originalname,type: mimetype, size: size })
+  })
 })
-
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+  console.log("Your app is listening on port " + port);
 });
